5fd12fe756cfb951b856c45a87330f49cfdfa02c
fortes23 <brais.fortes@gmail.com>
2019-03-14 19:52:30 +0000

    Add bashrc config to install script

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@
 DIR_REL=$(dirname $0)
 DIR=$(readlink -f $DIR_REL)
 
+# Source bashrc configurations
+if ! grep -q "conf/bashrc" ~/.bashrc; then
+    src_txt='# conf definition\n
+         if [ -d ~/conf/bashrc ]; then\n
+           \tfor f in ~/conf/bashrc/*; do\n
+             \t\t. $f\n
+           \tdone\n
+         fi\n'
+    echo -e $src_txt >> ~/.bashrc
+fi
+
+# Source git, svn and vim configurations
 if [ -f ~/.gitconfig ] || [ -f ~/.subversion/config ] || [ -f ~/.vimrc ]; then
 	while [ "$ANSWER" != "y" ] && [ "$ANSWER" != "n" ]
 	do
@@ -17,3 +29,4 @@ fi
 ln -sf $DIR/git/gitconfig ~/.gitconfig
 ln -sf $DIR/svn/svnconfig ~/.subversion/config
 ln -sf $DIR/vim/vimrc ~/.vimrc
+
